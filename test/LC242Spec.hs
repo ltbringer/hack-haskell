1d037127cf3b5c14cc75deabf0a8f938c75cbe62
ltbringer <amresh.venugopal@gmail.com>
2022-08-25 23:53:21 +0530

    test: LC242

---
+++ test/LC242Spec.hs
@@ -0,0 +1,14 @@
+module LC242Spec where
+
+import Test.Hspec
+import Lib242
+
+
+spec :: Spec
+spec = do
+  describe "Testing if 2 strings are anagrams" $ do
+    it "returns True since apple, lappe are anagrams" $ do
+      (isAnagram "apple" "lappe") `shouldBe` True
+
+    it "returns False since apple, appe are not anagrams" $
+      (isAnagram "apple" "appe") `shouldBe` False
