00261d6640285b5f7d596ce3d439a0fee0f38adb
ltbringer <amresh.venugopal@gmail.com>
2022-08-26 02:19:50 +0530

    test: LC11.

---
+++ test/LC11Spec.hs
@@ -0,0 +1,16 @@
+module LC11Spec where
+
+import Test.Hspec
+import Lib11
+import Data.Vector.Primitive as V
+
+spec :: Spec
+spec = do
+	describe "Maximum area stored between two pillars." $ do
+		let xs = V.fromList [1,8,6,2,5,4,8,3,7]
+		it "should return the maximum volume = 49" $ do
+			maxArea xs `shouldBe` 49
+
+		let xs = V.fromList [1,1]
+		it "should return the maximum volume = 1" $ do
+			maxArea xs `shouldBe` 1
