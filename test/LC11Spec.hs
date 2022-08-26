535060a950a6ac42af4e652206fcad1b28a0742c
ltbringer <amresh.venugopal@gmail.com>
2022-08-26 13:36:29 +0530

    test: add cases.

--- test/LC11Spec.hs
+++ test/LC11Spec.hs
@@ -7,10 +7,17 @@ import Data.Vector.Primitive as V
 spec :: Spec
 spec = do
 	describe "Maximum area stored between two pillars." $ do
-		let xs = V.fromList [1,8,6,2,5,4,8,3,7]
 		it "should return the maximum volume = 49" $ do
-			maxArea xs `shouldBe` 49
+			maxArea (V.fromList [1,8,6,2,5,4,8,3,7]) `shouldBe` 49
 
-		let ys = V.fromList [1,1]
 		it "should return the maximum volume = 1" $ do
-			maxArea ys `shouldBe` 1
+			maxArea (V.fromList [1,1]) `shouldBe` 1
+
+		it "should return the max area = 17" $ do
+			maxArea (V.fromList [5,18,17]) `shouldBe` 17
+
+		it "should return the max area = 17" $ do
+			maxArea (V.fromList [5,18,17]) `shouldBe` 17
+
+		it "should return the max area = 17" $ do
+			maxArea (V.fromList [2,3,4,5,18,17,6]) `shouldBe` 17
